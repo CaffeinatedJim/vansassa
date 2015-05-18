5abbe42e11265ec9b1ae49929a0b4cf4f579ca4f
Jim Birch <jim@xenomedia.com>
2015-05-18 16:25:21 -0500

    Updated

--- index.php
+++ index.php
@@ -1,152 +1,89 @@
-<html>
-<head>
-<title>Vansassa.com</title>
-<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/combo?2.9.0/build/reset-fonts-grids/reset-fonts-grids.css&2.9.0/build/base/base-min.css">
-<link href='http://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
-<style type="text/css">
-/*base.css */
-body {margin:0;padding:0;margin-top:-2em;}
-h1 {
-	font-size:167%;
-}
-h2 {
-	font-size:146.5%;
-}
-h3 {
-	font-size:146.5%;
-}
-h1, h2, h3 {
-	margin-bottom:1em;
-	color:#fff;
-}
-h1, h2, h3, strong, em, label {
-	font-weight:bold;
-}
-abbr, acronym {
-	border-bottom:1px dotted #fff;
-	cursor:help;
-}
-blockquote, ul, ol, dl {
-	margin:.5em;
-}
-ul li {
-	padding-bottom:.2em;
-}
-ol li {
-	list-style:decimal outside;
-}
-dl dd {
-	margin-left:.5em;
-	padding:.5em;
-}
-th, td {
-	border:1px solid #fff;
-	padding:.2em;
-}
-th, caption {
-	font-weight:bold;
-	text-align:center;
-}
-p, fieldset, pre {
-	margin-bottom:.5em;
-}
-/* Base styles, mainly menu lists corrections, probably leave as is */
-a img {
-	border:none;
-}
-blockquote {
-	border-left:5px solid #ccc;
-	margin-left:1.5em;
-	padding-left:5px;
-}
-#commentform textarea {
-	height:100px;
-	width:100%;
-}
-ol#commentlist li {
-	list-style-type:none;
-}
-ol#commentlist li li {
-	list-style-type:inherit;
-}
-#menu ul li {
-	font-weight:bold;
-	list-style-type:none;
-	padding-top:26px;
-}
-#menu ul ul {
-	padding-top:0;
-	margin-top:0;
-	margin-bottom:0;
-}
-#menu ul ul li {
-	font-weight:normal;
-	list-style-type:disc;
-	padding:0;
-	margin:0;
-}
-body {
-	background: #000 url(stuff/vansassa-parade-bg.jpg) top center no-repeat;
-}
-#hd {
-	background:#999999;text-align:center;}
-#bd {
-	background:#999999;padding:1em;
-	font-family: 'Montserrat', sans-serif;font-size:131%;
-	}
-.album img{float:right;margin-left:1em;margin-bottom:1em;}
-.style1 {color: #DD938E}
-</style>
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Vansassa.com</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+    <link href='http://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <style type="text/css">
+        body {
+            font-family: 'Montserrat', sans-serif;
+            font-size:20px;
+        	background: #000 url(stuff/vansassa-parade-bg.jpg) top center no-repeat;
+        }
+        .hd {margin:0;}
+        .hd img {width: 100%;height: auto;}
+        .bd {background:#999999; padding-bottom: 1em;margin-left: 15px;margin-right: 15px;}
+        .embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; }
+        .embed-container-bandcamp { position: relative; padding-bottom: 156.25%; height: 0; overflow: hidden; max-width: 100%; }
+        .embed-container iframe, .embed-container-bandcamp iframe, .embed-container object, .embed-container-bandcamp object, .embed-container embed, .embed-container-bandcamp embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }
+    </style>
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-9711088-10', 'auto');
-  ga('send', 'pageview');
-
-</script>
-</head>
-<body>
-<div id="doc4" class="yui-t7">
-<div id="hd" role="banner">
-  <h1><img src="http://f1.bcbits.com/img/0000420118_100.png" alt="Vansassa" /></h1>
-</div>
-<div id="bd" role="main">
-  <div class="yui-g">
-    <h2>You can listen</h2>
-    <iframe width="300" height="355" style="float:left;display: block; width: 300px; height: 355px;margin-right:2em;" src="http://bandcamp.com/EmbeddedPlayer/v=2/album=3425327455/size=grande2/bgcol=999999/linkcol=660099/" allowtransparency="true" frameborder="0"><a href="http://vansassa.bandcamp.com/album/songs-to-sing-to-the-children">Songs to Sing to the Children by Vansassa</a></iframe>
-    <p><strong>Listen or download Vansassa 7 song album<br />"<a href="http://vansassa.bandcamp.com/album/songs-to-sing-to-the-children" title="Songs to Sing to the Children">Songs to Sing to the Children</a>" for free!</strong></p>
-    <div class="album"><a href="http://vansassa.bandcamp.com/album/songs-to-sing-to-the-children" title="Songs to Sing to the Children"><img src="http://f1.bcbits.com/img/a2548182645_2.jpg" title="Vansassa Songs to Sing to the Children" alt="Vansassa Songs to Sing to the Children" /></a></div>
-    <p>It is the good. Vansassa is recording artist from great city of Chicago. She is traveler of first class in airplane of around the world thinking about cats that bring her inspiration of the music. This album is now in existence because children do not listen worse than the monkey. Children should clean house good.</p>
-    <p>Vansassa had help of many people who will do as she say, not as she does, because is impossible for average person to achieve. It is ok. So now maybe you and your children will listen to her as well. Children should be quiet in clean room early to bed. She would like to thank you the fans, and for them to know if they have nothing nice to say, then say it to the children. </p>
-    <span class="style1"></span>
-  </div>
-  <div class="yui-g">
-    <div class="yui-u first">
-      <a href="http://www.facebook.com/pages/Vansassa/100841656680521" title="Vansassa on Facebook"><img src="stuff/facebook-icon.png" alt="Like Vansassa on Facebook" style="float:left;margin-right:1em;"></a>
-      <h2>You can like</h2>
-      <p>Like <a href="http://www.facebook.com/pages/Vansassa/100841656680521" title="Vansassa on Facebook">Vansassa on Facebook</a></p>
-    </div>
-    <div class="yui-u">
-      <a href="http://www.midwestmerch.net/detail/vansassa-vansassa-parade/" title="Get Vansassa Shirt!"><img src="stuff/vansassa-shirt.jpg" alt="Get Vansassa Shirt!" style="float:left;margin-right:1em;width:256px;height:341px;"></a>
-      <h3>You can wear</h3>
-      <p>You can show your new found love of Vansassa with the special <a href="http://www.midwestmerch.net/detail/vansassa-vansassa-parade/" title="Get Vansassa Shirt!">Vansassa Diamond Parade T-Shirt</a>!  You can order one now.</p>
-    </div>
-  </div>
-      <div class="yui-g">
-        <h2>You can watch</h2>
-        <p>Vansassa, her cousin Igor, and their Japanese grandfather sing about bubble boy Falcon Heene after he flew over into the studio in his spaceship.  We're all aliens.</p>
-        <div style="margin-left:auto;margin-right:auto;margin-bottom:2em;text-align:center;"><iframe width="853" height="480" src="//www.youtube.com/embed/gWd32HlYLQk" frameborder="0" allowfullscreen></iframe></div>
-        <p>Watch Vansassa song "Stuff My Stocking" on youtube.  Song was entry in 2011 Moog Music Animoog Holiday Song Contest.</p>
-        <div style="margin-left:auto;margin-right:auto;margin-bottom:2em;text-align:center;"><iframe width="853" height="480" src="http://www.youtube.com/embed/z4rgxIi9RTk" frameborder="0" allowfullscreen></iframe></div>
-        <p>Vanda y Marek do dramatic interpretation of Secret Letter Sent to Pierogi Party.  You better hide your jewelry.</p>
-        <div style="margin-left:auto;margin-right:auto;margin-bottom:2em;text-align:center;"><iframe width="853" height="480" src="//www.youtube.com/embed/iMecCJw09sM" frameborder="0" allowfullscreen></iframe></div>
-      </div>
-  <div id="ft" role="contentinfo">
-    <p>&nbsp;</p>
-  </div>
-</div>
-</body>
+        ga('create', 'UA-9711088-10', 'auto');
+        ga('send', 'pageview');
+    </script>
+    </head>
+    <body>
+        <div class="container">
+            <div class="row hd">
+                <div class="col-sm-12">
+                    <img src="http://f1.bcbits.com/img/0000420118_100.png" alt="Vansassa" />
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row bd">
+                <div class="col-sm-8">
+                    <h2>You can listen</h2>
+                    <p><strong>Listen or download Vansassa 7 song album<br />"<a href="http://vansassa.bandcamp.com/album/songs-to-sing-to-the-children" title="Songs to Sing to the Children">Songs to Sing to the Children</a>" for free!</strong></p>
+                    <p>It is the good. Vansassa is recording artist from great city of Chicago. She is traveler of first class in airplane of around the world thinking about cats that bring her inspiration of the music. This album is now in existence because children do not listen worse than the monkey. Children should clean house good.</p>
+                    <p>Vansassa had help of many people who will do as she say, not as she does, because is impossible for average person to achieve. It is ok. So now maybe you and your children will listen to her as well. Children should be quiet in clean room early to bed. She would like to thank you the fans, and for them to know if they have nothing nice to say, then say it to the children.</p>
+                </div>
+                <div class="col-sm-4">
+                    <div class='embed-container-bandcamp'><iframe style='border: 0;' src='https://bandcamp.com/EmbeddedPlayer/album=3425327455/size=large/bgcol=ffffff/linkcol=333333/tracklist=false/transparent=true/' seamless><a href='http://vansassa.bandcamp.com/album/songs-to-sing-to-the-children'>Songs to Sing to the Children by Vansassa</a></iframe></div>
+                </div>
+            </div>
+            <div class="row bd">
+                <div class="col-sm-6">
+                  <a href="http://www.facebook.com/pages/Vansassa/100841656680521" title="Vansassa on Facebook"><img src="stuff/facebook-icon.png" alt="Like Vansassa on Facebook" style="float:left;margin-right:1em;"></a>
+                  <h2>You can like</h2>
+                  <p>Like <a href="http://www.facebook.com/pages/Vansassa/100841656680521" title="Vansassa on Facebook">Vansassa on Facebook</a></p>
+                </div>
+                <div class="col-sm-6">
+                      <a href="http://www.midwestmerch.net/detail/vansassa-vansassa-parade/" title="Get Vansassa Shirt!"><img src="stuff/vansassa-shirt.jpg" alt="Get Vansassa Shirt!" style="float:left;margin-right:1em;width:256px;height:341px;"></a>
+                      <h3>You can wear</h3>
+                      <p>You can show your new found love of Vansassa with the special <a href="http://www.midwestmerch.net/detail/vansassa-vansassa-parade/" title="Get Vansassa Shirt!">Vansassa Diamond Parade T-Shirt</a>!  You can order one now.</p>
+                </div>
+            </div>
+            <div class="row bd">
+                <div class="col-sm-12">
+                    <h2>You can watch</h2>
+                    <p>Vansassa, her cousin Igor, and their Japanese grandfather sing about bubble boy Falcon Heene after he flew over into the studio in his spaceship.  We're all aliens.</p>
+                    <div class='embed-container'><iframe src='http://www.youtube.com/embed/gWd32HlYLQk' frameborder='0' allowfullscreen></iframe></div>
+                    <p>Watch Vansassa song "Stuff My Stocking" on youtube.  Song was entry in 2011 Moog Music Animoog Holiday Song Contest.</p>
+                    <div class='embed-container'><iframe src='http://www.youtube.com/embed/z4rgxIi9RTk' frameborder='0' allowfullscreen></iframe></div>
+                    <p>Vanda y Marek do dramatic interpretation of Secret Letter Sent to Pierogi Party.  You better hide your jewelry.</p>
+                    <div class='embed-container'><iframe src='http://www.youtube.com/embed/iMecCJw09sM' frameborder='0' allowfullscreen></iframe></div>
+                </div>
+          </div>
+        </div>
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="js/bootstrap.min.js"></script>
+    </body>
 </html>
