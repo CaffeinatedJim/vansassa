199cd61970acb8d74ada5cf34defb1d0aa380376
Jim Birch <git@jimbir.ch>
2024-04-20 07:51:37 -0400

    Lighthouse improvements

--- index.php
+++ index.php
@@ -5,29 +5,23 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Vansassa.com</title>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+        <meta name="description" content="Vansassa website, you like.">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css" defer>
         <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
-        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-        <!--[if lt IE 9]>
-          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-        <![endif]-->
         <style type="text/css">
             body {
                 font-family: 'Montserrat', sans-serif;
                 font-size: 20px;
-            	background: #000 url(stuff/vansassa-parade-bg.jpg) top center no-repeat;
+            	background: #000 url(stuff/vansassa-parade-bg.webp) top center no-repeat;
             }
             .hd {margin: 0;}
             .hd img {width: 100%; height: auto;}
-            .bd {background:#999999; padding-bottom: 1em;margin-left: 15px;margin-right: 15px;}
+            .bd {background:#ededed; padding-bottom: 1em;margin-left: 15px;margin-right: 15px;}
             .embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; }
             .embed-container-bandcamp { position: relative; padding-bottom: 156.25%; height: 0; overflow: hidden; max-width: 100%; }
             .embed-container iframe, .embed-container-bandcamp iframe, .embed-container object, .embed-container-bandcamp object, .embed-container embed, .embed-container-bandcamp embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }
         </style>
-        <script>
+        <script defer>
             (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
             (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
             m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -37,7 +31,7 @@
             ga('send', 'pageview');
         </script>
         <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-        <script>
+        <script defer>
              (adsbygoogle = window.adsbygoogle || []).push({
                   google_ad_client: "ca-pub-8768395496150142",
                   enable_page_level_ads: true
@@ -48,20 +42,20 @@
         <div class="container">
             <div class="row hd">
                 <div class="col-sm-12">
-                    <img src="https://f1.bcbits.com/img/0000420118_100.png" alt="Vansassa" />
+                    <img src="stuff/vh.webp" alt="Vansassa header, you know." />
                 </div>
             </div>
         </div>
         <div class="container">
             <div class="row bd">
                 <div class="col-sm-8">
-                    <h2>You can listen</h2>
+                    <h2>Vansassa songs, you listen.</h2>
                     <p><strong>Listen or download Vansassa 7 song album<br />"<a href="https://vansassa.bandcamp.com/album/songs-to-sing-to-the-children" title="Songs to Sing to the Children">Songs to Sing to the Children</a>" for free!</strong></p>
                     <p>It is the good. Vansassa is recording artist from great city of Chicago. She is traveler of first class in airplane of around the world thinking about cats that bring her inspiration of the music. This album is now in existence because children do not listen worse than the monkey. Children should clean house good.</p>
                     <p>Vansassa had help of many people who will do as she say, not as she does, because is impossible for average person to achieve. It is ok. So now maybe you and your children will listen to her as well. Children should be quiet in clean room early to bed. She would like to thank you the fans, and for them to know if they have nothing nice to say, then say it to the children.</p>
                 </div>
                 <div class="col-sm-4">
-                    <div class='embed-container-bandcamp'><iframe style='border: 0;' src='https://bandcamp.com/EmbeddedPlayer/album=3425327455/size=large/bgcol=ffffff/linkcol=333333/tracklist=false/transparent=true/' seamless><a href='https://vansassa.bandcamp.com/album/songs-to-sing-to-the-children'>Songs to Sing to the Children by Vansassa</a></iframe></div>
+                    <div class='embed-container-bandcamp'><iframe style='border: 0;' src='https://bandcamp.com/EmbeddedPlayer/album=3425327455/size=large/bgcol=ffffff/linkcol=333333/tracklist=false/transparent=true/' seamless title="Vansassa songs, you listen, from camp band."><a href='https://vansassa.bandcamp.com/album/songs-to-sing-to-the-children'>Songs to Sing to the Children by Vansassa</a></iframe></div>
                 </div>
             </div>
             <!--
